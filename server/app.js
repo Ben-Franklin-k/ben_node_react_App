656a143a3b46e44b165bd68c5ffd13e5879e4c3f
Ben Franklin <benfranklin619@gmail.com>
2024-02-05 16:01:49 +0530

    added path to serve static content

--- server/app.js
+++ server/app.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const cors = require("cors")
+const path = require("path")
+
 
 const app = express()
 const port = 4000
 app.use(cors())
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
 
 app.get('/api/', (req, res) => {
     return res.status(200).json({
         data: "Express"
     })
 })
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '..', "client/build", "index.html"))
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+console.log(path.join(__dirname, '..', "client/build", "index.html"))
